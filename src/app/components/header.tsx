4950b23e3eae1f667326130c047eef4ec0b18bad
TM_Brodie <brodie.balser@ticketmaster.com>
2026-06-17 10:30:06 -0400

    hide menu button on select-user page

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 interface HeaderProps {
   isDrawerOpen: boolean
@@ -8,14 +9,19 @@ interface HeaderProps {
 }
 
 export default function Header({ isDrawerOpen, onMenuToggle }: HeaderProps) {
+  const pathname = usePathname()
+  const isSelectUser = pathname === '/'
+
   return (
     <button
-      onClick={onMenuToggle}
+      onClick={isSelectUser ? undefined : onMenuToggle}
       className="fixed left-0 top-0 z-50 flex h-14 w-full items-center justify-between border-b-4 border-on-surface bg-background px-6"
       id="app-header"
     >
       <div className="flex items-center gap-4" id="header-branding">
-        <span className="material-symbols-outlined text-primary">menu</span>
+        {!isSelectUser && (
+          <span className="material-symbols-outlined text-primary">menu</span>
+        )}
         <h1 className="font-headline-lg text-headline-lg font-black uppercase tracking-tighter text-primary">
           Kachalka
         </h1>
